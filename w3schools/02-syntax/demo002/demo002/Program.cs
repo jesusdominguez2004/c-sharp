b81b406ae49446ca33cc6d0f793e24f1bbc9d68f
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-06-30 17:21:42 -0500

    syntax w3schools

---
+++ w3schools/02-syntax/demo002/demo002/Program.cs
@@ -0,0 +1,13 @@
+﻿// W3Schools, C# Syntax
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
