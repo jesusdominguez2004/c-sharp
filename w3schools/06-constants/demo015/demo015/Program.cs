fd5ab4e7eca44a15c570a7e575fb443931c2c237
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-06-30 20:28:29 -0500

    constants w3schools

---
+++ w3schools/06-constants/demo015/demo015/Program.cs
@@ -0,0 +1,14 @@
+﻿// W3Schools, C# Variables, Constants
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int myNum = 15;
+            Console.WriteLine(myNum);
+        }
+    }
+}
