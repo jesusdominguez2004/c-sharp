eee83e43c51f51e5f7c48321657987fc5efa172f
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-06-30 20:50:39 -0500

    identifiers w3schools

---
+++ w3schools/09-indentifiers/demo021/demo021/Program.cs
@@ -0,0 +1,17 @@
+﻿// W3Schools, C# Variables, Identifiers
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutesPerHour = 60;    // Good
+            int m = 60;                 // Ok, but not so easy to understand what m actually is
+
+            Console.WriteLine(minutesPerHour);
+            Console.WriteLine(m);
+        }
+    }
+}
