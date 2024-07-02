7f5a6eaf5af64f11499c6a0539feaf6959035af5
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-07-01 20:36:55 -0500

    logical operators w3schools

---
+++ w3schools/16-logical-operators/demo042/demo042/Program.cs
@@ -0,0 +1,16 @@
+﻿// W3Schools, C# Operators, Logical
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            Console.WriteLine(x < 5 && x < 10);     // Logical and (returns True)
+            Console.WriteLine(x < 5 || x < 4);      // Logical or  (returns True)
+            Console.WriteLine(!(x < 5 && x < 10));  // Logical not (returns False)
+        }
+    }
+}
