7401e533f23af6f61f427037c1d7f9bbf5ee1bb6
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-07-01 20:20:36 -0500

    assignment operators w3schools

---
+++ w3schools/14-assignment-operators/demo038/demo038/Program.cs
@@ -0,0 +1,18 @@
+﻿// W3Schools, C# Operators, Assignment
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 10;
+            y += 5;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+    }
+}
---
+++ w3schools/14-assignment-operators/demo039/demo039/Program.cs
@@ -0,0 +1,87 @@
+﻿// W3Schools, C# Operators, Assignment
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            
+            // =
+            x = 5;
+            Console.WriteLine(x);
+
+            // +=
+            x += 3;
+            Console.WriteLine(x);
+
+            x = x + 3;
+            Console.WriteLine(x);
+
+            // -=
+            x -= 3;
+            Console.WriteLine(x);
+
+            x = x - 3;
+            Console.WriteLine(x);
+
+            // *=
+            x *= 3;
+            Console.WriteLine(x);
+
+            x = x * 3;
+            Console.WriteLine(x);
+
+            // /=
+            x /= 3;
+            Console.WriteLine(x);
+
+            x = x / 3;
+            Console.WriteLine(x);
+
+            // %=
+            x %= 3;
+            Console.WriteLine(x);
+
+            x = x % 3;
+            Console.WriteLine(x);
+
+            // &=
+            x &= 3;
+            Console.WriteLine(x);
+
+            x = x & 3;
+            Console.WriteLine(x);
+
+            // |=
+            x |= 3;
+            Console.WriteLine(x);
+
+            x = x | 3;
+            Console.WriteLine(x);
+
+            // ^=
+            x ^= 3;
+            Console.WriteLine(x);
+
+            x = x ^ 3;
+            Console.WriteLine(x);
+
+            // >>=
+            x >>= 3;
+            Console.WriteLine(x);
+
+            x = x >> 3;
+            Console.WriteLine(x);
+
+            // <<=
+            x <<= 3;
+            Console.WriteLine(x);
+
+            x = x << 3;
+            Console.WriteLine(x);
+        }
+    }
+}
