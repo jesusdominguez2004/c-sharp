d980c61d42f50d3fe967f40eca0fe6261435f4a1
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-07-01 20:31:06 -0500

    comparison operators w3schools

---
+++ w3schools/15-comparison-operators/demo040/demo040/Program.cs
@@ -0,0 +1,15 @@
+﻿// W3Schools, C# Operators, Comparison
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 3;
+            Console.WriteLine(x > y);   // returns True
+        }
+    }
+}
---
+++ w3schools/15-comparison-operators/demo041/demo041/Program.cs
@@ -0,0 +1,21 @@
+﻿// W3Schools, C# Operators, Comparison
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 3;
+            Console.WriteLine(x > y);   // returns True
+            Console.WriteLine(x == y);  // returns False
+            Console.WriteLine(x != y);  // returns True
+            Console.WriteLine(x > y);   // returns True
+            Console.WriteLine(x < y);   // returns False
+            Console.WriteLine(x >= y);  // returns True
+            Console.WriteLine(x <= y);  // returns False
+        }
+    }
+}
