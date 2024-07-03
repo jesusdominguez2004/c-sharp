fa2af55bc7762ce7fcde4b70cee21e5269b97a42
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-07-03 18:46:31 -0500

    concatenation w3schools

---
+++ w3schools/19-concatenation/demo048/demo048/Program.cs
@@ -0,0 +1,16 @@
+﻿// W3Schools, C# Strings, Concatenation
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "John ";
+            string lastName = "Doe";
+            string name = firstName + lastName;
+            Console.WriteLine(name);
+        }
+    }
+}
---
+++ w3schools/19-concatenation/demo050/demo050/Program.cs
@@ -0,0 +1,21 @@
+﻿// W3Schools, C# Strings, Concatenation
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+            int z = x + y;
+            Console.WriteLine(z);   // z will be 30 (an integer/number)
+
+            string a = "10";
+            string b = "20";
+            string c = a + b;
+            Console.WriteLine(c);  // c will be 1020 (a string)
+        }
+    }
+}
