0af67b868219a689612349bee05d01e6d82f805f
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-07-04 19:48:22 -0500

    interpolation w3schools

---
+++ w3schools/20-interpolation/demo051/demo051/Program.cs
@@ -0,0 +1,16 @@
+﻿// W3Schools, C# Strings, Interpolation
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "John";
+            string lastName = "Doe";
+            string name = $"My full name is: {firstName} {lastName}";
+            Console.WriteLine(name);
+        }
+    }
+}
