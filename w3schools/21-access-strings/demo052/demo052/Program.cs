05b6404040aebfe19d1c3a7a35508df9479ea3ce
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-07-04 22:10:36 -0500

    access strings w3schools

---
+++ w3schools/21-access-strings/demo052/demo052/Program.cs
@@ -0,0 +1,24 @@
+﻿// W3Schools, C# Strings, Access Strings
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "Hello";
+            Console.WriteLine(myString[0]); // Outputs "H"
+            Console.WriteLine(myString[1]); // Outputs "e"
+            Console.WriteLine(myString[2]); // Outputs "l"
+            Console.WriteLine(myString[3]); // Outputs "l"
+            Console.WriteLine(myString[4]); // Outputs "o"
+
+            Console.WriteLine(myString.IndexOf("H"));       // Outputs 0
+            Console.WriteLine(myString.IndexOf("e"));       // Outputs 1
+            Console.WriteLine(myString.IndexOf("l"));       // Outputs 2
+            Console.WriteLine(myString.LastIndexOf("l"));   // Outputs 3
+            Console.WriteLine(myString.IndexOf("o"));       // Outputs 4
+        }
+    }
+}
---
+++ w3schools/21-access-strings/demo053/demo053/Program.cs
@@ -0,0 +1,19 @@
+﻿// W3Schools, C# Strings, Access Strings
+using System;
+
+namespace GetLastName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "John Doe";
+
+            int charPos = name.IndexOf("D");
+
+            string lastName = name.Substring(charPos);
+
+            Console.WriteLine(lastName);
+        }
+    }
+}
