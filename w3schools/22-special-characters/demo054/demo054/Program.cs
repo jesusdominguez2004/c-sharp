c40e6bbbddce7a9468db999b3a59d680ef5d7244
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-07-04 22:27:43 -0500

    special characters w3schools

---
+++ w3schools/22-special-characters/demo054/demo054/Program.cs
@@ -0,0 +1,19 @@
+﻿// W3Schools, C# Strings, Special Characters
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt1 = "We are the so-called \"Vikings\" from the north.";
+            string txt2 = "It\'s alright.";
+            string txt3 = "The character \\ is called backslash.";
+
+            Console.WriteLine(txt1);
+            Console.WriteLine(txt2);
+            Console.WriteLine(txt3);
+        }
+    }
+}
---
+++ w3schools/22-special-characters/demo055/demo055/Program.cs
@@ -0,0 +1,19 @@
+﻿// W3Schools, C# Strings, Special Characters
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt1 = "Hello\nWorld!";
+            string txt2 = "Hello\tWorld!";
+            string txt3 = "Hel\blo World!";
+
+            Console.WriteLine(txt1);
+            Console.WriteLine(txt2);
+            Console.WriteLine(txt3);
+        }
+    }
+}
