081aaff15fc22870d4041ee3576e3d1e2bbc1246
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-07-07 18:59:35 -0500

    if w3schools

---
+++ w3schools/24-if/demo060/demo060/Program.cs
@@ -0,0 +1,16 @@
+﻿// W3Schools, C# If...Else, if
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (20 > 18)
+            {
+                Console.WriteLine("20 is greater than 18");
+            }
+        }
+    }
+}
