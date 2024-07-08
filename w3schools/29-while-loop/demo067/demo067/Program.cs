96827e5f4fe05481ea51df34d359a982695eba76
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-07-07 19:36:59 -0500

    while loop w3schools

---
+++ w3schools/29-while-loop/demo067/demo067/Program.cs
@@ -0,0 +1,18 @@
+﻿// W3Schools, C# While Loop
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
