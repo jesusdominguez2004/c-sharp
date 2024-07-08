66d85f29284363dbbf35f96cfa3d9df7891cc678
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-07-07 19:27:07 -0500

    switch w3schools

---
+++ w3schools/28-switch/demo065/demo065/Program.cs
@@ -0,0 +1,37 @@
+﻿// W3Schools, C# Switch
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = 4;
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+            }
+        }
+    }
+}
---
+++ w3schools/28-switch/demo066/demo066/Program.cs
@@ -0,0 +1,25 @@
+﻿// W3Schools, C# Switch
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = 4;
+            switch (day)
+            {
+                case 6:
+                    Console.WriteLine("Saturday.");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday.");
+                    break;
+                default:
+                    Console.WriteLine("Looking forward to the Weekend.");
+                    break;
+            }
+        }
+    }
+}
