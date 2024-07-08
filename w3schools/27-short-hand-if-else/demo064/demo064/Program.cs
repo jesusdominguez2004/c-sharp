45eb7342f9fe70fdc4e4b2d3d190a6696f07572e
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-07-07 19:14:35 -0500

    short hand if else w3schools

---
+++ w3schools/27-short-hand-if-else/demo064/demo064/Program.cs
@@ -0,0 +1,16 @@
+﻿// W3Schools, C# If...Else, Short hand if...else
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variable = (condition) ? expressionTrue : expressionFalse;
+            int time = 20;
+            string result = (time < 18) ? "Good day." : "Good evening.";
+            Console.WriteLine(result);
+        }
+    }
+}
