c59b7c4afb274a6740062f53fcf1f3d018324bb3
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-07-07 19:02:57 -0500

    else w3schools

---
+++ w3schools/25-else/demo062/demo062/Program.cs
@@ -0,0 +1,21 @@
+﻿// W3Schools, C# If...Else, else
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = 20;
+            if (time < 18)
+            {
+                Console.WriteLine("Good day.");
+            }
+            else
+            {
+                Console.WriteLine("Good evening.");
+            }
+        }
+    }
+}
