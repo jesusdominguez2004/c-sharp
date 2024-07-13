5f85ee98e69f2c31a2b72bd6cb801abbeffdf5ed
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-07-12 19:04:54 -0500

    sort arrays w3schools

---
+++ w3schools/33-arrays/demo085/demo085/Program.cs
@@ -0,0 +1,27 @@
+﻿// W3Schools, C# Arrays, Sort arrays
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            // Sort a string
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            Array.Sort(cars);
+            foreach (string i in cars)
+            {
+                Console.WriteLine(i);
+            }
+
+            // Sort a int
+            int[] numbers = { 5, 1, 8, 9 };
+            Array.Sort(numbers);
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ w3schools/33-arrays/demo086/demo086/Program.cs
@@ -0,0 +1,18 @@
+﻿// W3Schools, C# Arrays, Sort arrays
+using System;
+using System.Linq;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Min, Max, and Sum, can be found in the System.Linq namespace:
+            int[] myNumbers = { 5, 1, 8, 9 };
+            Console.WriteLine(myNumbers.Max()); // returns the largest value
+            Console.WriteLine(myNumbers.Min()); // returns the smallest value
+            Console.WriteLine(myNumbers.Sum()); // returns the sum of elements
+        }
+    }
+}
