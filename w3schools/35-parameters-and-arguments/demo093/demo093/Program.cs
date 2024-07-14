3924cb101d30d3413a569d82c809d327f7198fe8
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-07-13 20:34:21 -0500

    parameters & arguments w3schools

---
+++ w3schools/35-parameters-and-arguments/demo093/demo093/Program.cs
@@ -0,0 +1,22 @@
+﻿// W3Schools, C# Method Parameters, Parameters & Arguments
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        // fname is a parameter
+        static void MyMethod(string fname)
+        {
+            Console.WriteLine(fname + " Refsnes");
+        }
+
+        // Liam, Jenny, Anja are arguments
+        static void Main(string[] args)
+        {
+            MyMethod("Liam");
+            MyMethod("Jenny");
+            MyMethod("Anja");
+        }
+    }
+}
---
+++ w3schools/35-parameters-and-arguments/demo094/demo094/Program.cs
@@ -0,0 +1,22 @@
+﻿// W3Schools, C# Method Parameters, Parameters & Arguments
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        // fname is a parameter
+        static void MyMethod(string fname, int age)
+        {
+            Console.WriteLine(fname + " is " + age);
+        }
+
+        // Liam, Jenny, Anja are arguments
+        static void Main(string[] args)
+        {
+            MyMethod("Liam", 5);
+            MyMethod("Jenny", 8);
+            MyMethod("Anja", 31);
+        }
+    }
+}
