ba0e3fd19d80f22b943b79d068843cf4ca6717dc
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-07-14 16:14:26 -0500

    method overloading w3schools

---
+++ w3schools/39-method-overloading/demo101/demo101/Program.cs
@@ -0,0 +1,26 @@
+﻿// W3Schools, C# Method Overloading
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static int PlusMethod(int x, int y)
+        {
+            return x + y;
+        }
+
+        static double PlusMethod(double x, double y)
+        {
+            return x + y;
+        }
+
+        static void Main(string[] args)
+        {
+            int myNum1 = PlusMethod(8, 5);
+            double myNum2 = PlusMethod(4.3, 6.26);
+            Console.WriteLine("Int: " + myNum1);
+            Console.WriteLine("Double: " + myNum2);
+        }
+    }
+}
