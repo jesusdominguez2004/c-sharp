17363e69dcfa7a012a9de00e6946faca5ee6c7fe
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-07-13 20:35:37 -0500

    default parameter w3schools

---
+++ w3schools/36-default-parameter/demo095/demo095/Program.cs
@@ -0,0 +1,24 @@
+﻿// W3Schools, C# Method Parameters, Default Parameter
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        // country is a parameter
+        // Norway is the dedault parameter
+        static void MyMethod(string country = "Norway")
+        {
+            Console.WriteLine(country);
+        }
+
+        // Sweden, India, Norway, USA are arguments
+        static void Main(string[] args)
+        {
+            MyMethod("Liam");
+            MyMethod("Jenny");
+            MyMethod();
+            MyMethod("Anja");
+        }
+    }
+}
