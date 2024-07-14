bdb36523bf9c7078e545cd1d8fe14e4a0b65a86f
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-07-14 16:05:35 -0500

    named arguments w3schools

---
+++ w3schools/38-named-arguments/demo099/demo099/Program.cs
@@ -0,0 +1,18 @@
+﻿// W3Schools, C# Method Parameters, Named Arguments
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void MyMethod(string child1, string child2, string child3)
+        {
+            Console.WriteLine("The youngest child is: " + child3);
+        }
+
+        static void Main(string[] args)
+        {
+            MyMethod(child3: "John", child1: "Liam", child2: "Liam");
+        }
+    }
+}
