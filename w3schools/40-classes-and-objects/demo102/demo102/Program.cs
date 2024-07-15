b72339f4640ccc7ec1bd6a81148df536f95f46cb
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-07-15 18:07:56 -0500

    classes & objects w3schools

---
+++ w3schools/40-classes-and-objects/demo102/demo102/Program.cs
@@ -0,0 +1,16 @@
+﻿// W3Schools, C# Classes/Objects, Classes and Objects
+using System;
+
+namespace MyApplication
+{
+    class Car
+    {
+        string color = "red";
+
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            Console.WriteLine(myObj.color);
+        }
+    }
+}
