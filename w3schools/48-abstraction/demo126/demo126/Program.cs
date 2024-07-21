99c0e29791d462e45052cdb059b4212387245dbd
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-07-20 20:24:17 -0500

    abstraction w3schools

---
+++ w3schools/48-abstraction/demo126/demo126/Program.cs
@@ -0,0 +1,38 @@
+﻿// W3Schools, C# Abstraction
+using System;
+
+namespace MyApplication
+{
+    // Abstract class
+    abstract class Animal
+    {
+        // Abstract method (does not have a body)
+        public abstract void AnimalSound();
+
+        // Regular method
+        public void Sleep()
+        {
+            Console.WriteLine("Zzz");
+        }
+    }
+
+    // Derived class (inherit from Animal)
+    class Pig : Animal
+    {
+        public override void AnimalSound()
+        {
+            // The body of animalSound() is provided here
+            Console.WriteLine("The pig says: wee wee");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pig myPig = new Pig();  // Create a Pig object
+            myPig.AnimalSound();    // Call the abstract method
+            myPig.Sleep();          // Call the regular method
+        }
+    }
+}
