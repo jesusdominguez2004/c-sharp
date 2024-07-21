cdabd20b8dffc709fd20038aa64ca572d72d6f70
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-07-20 20:09:49 -0500

    polymorphism w3schools

---
+++ w3schools/47-polymorphism/demo125/demo125/Program.cs
@@ -0,0 +1,43 @@
+﻿// W3Schools, C# Polymorphism
+using System;
+
+namespace MyApplication
+{
+    class Animal
+    {
+        public virtual void AnimalSound()
+        {
+            Console.WriteLine("The animal makes a sound");
+        }
+    }
+
+    class Pig : Animal
+    {
+        public override void AnimalSound()
+        {
+            Console.WriteLine("The pig says: wee wee");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void AnimalSound()
+        {
+            Console.WriteLine("The dog says: bow bow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myAnimal = new Animal();
+            Animal myPig = new Pig();
+            Animal myDog = new Dog();
+
+            myAnimal.AnimalSound();
+            myPig.AnimalSound();
+            myDog.AnimalSound();
+        }
+    }
+}
