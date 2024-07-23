b92f6173a1f3e44e6b03b280885491cdf190dbdb
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-07-22 19:58:13 -0500

    files w3schools

---
+++ w3schools/51-files/demo134/demo134/Program.cs
@@ -0,0 +1,18 @@
+﻿// W3Schools, C# Files
+using System;
+using System.IO;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string writeText = "Hello World!";
+            File.WriteAllText("filename.txt", writeText);
+
+            string readText = File.ReadAllText("filename.txt");
+            Console.WriteLine(readText);
+        }
+    }
+}
