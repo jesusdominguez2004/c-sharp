66cd13999f62ae7f831ebea9c402c40ed50331d7
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-07-22 19:34:26 -0500

    interface w3schools

---
+++ w3schools/49-interface/demo127/demo127/Program.cs
@@ -0,0 +1,30 @@
+﻿// W3Schools, C# Interface, Interface
+using System;
+
+namespace MyApplication
+{
+    // Interface
+    interface IAnimal
+    {
+        void AnimalSound(); // interface method (does not have a body)
+    }
+
+    // Pig "implements" the IAnimal interface
+    class Pig : IAnimal
+    {
+        // The body of AnimalSound()
+        public void AnimalSound()
+        {
+            Console.WriteLine("The pig says: wee wee");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pig myPig = new Pig();
+            myPig.AnimalSound();
+        }
+    }
+}
---
+++ w3schools/49-interface/demo128/demo128/Program.cs
@@ -0,0 +1,38 @@
+﻿// W3Schools, C# Interface, Multiple Interfaces
+using System;
+
+namespace MyApplication
+{
+    interface IFirstInterface
+    {
+        void MyMethod();
+    }
+
+    interface ISecondInterface
+    {
+        void MyOtherMethod();
+    }
+
+    // Implement multiple interfaces
+    class DemoClass : IFirstInterface, ISecondInterface
+    {
+        public void MyMethod()
+        {
+            Console.WriteLine("Some text...");
+        }
+        public void MyOtherMethod()
+        {
+            Console.WriteLine("Some other text...");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoClass myObj = new DemoClass();
+            myObj.MyMethod();
+            myObj.MyOtherMethod();
+        }
+    }
+}
