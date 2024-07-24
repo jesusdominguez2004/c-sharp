e36bde2645798495015ec40c96ec989e2cb32365
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-07-23 19:30:20 -0500

    how to w3schools

---
+++ w3schools/53-how-to/demo139/demo139/Program.cs
@@ -0,0 +1,16 @@
+﻿// W3Schools, C# How To, Add Two Numbers
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 6;
+            int sum = x + y;
+            Console.WriteLine(sum);
+        }
+    }
+}
